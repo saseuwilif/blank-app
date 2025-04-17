d2ea633f6a4a4785c98ef843710cc234f5ecc5aa
saseuwilif <saseuwilif@gmail.com>
2025-04-17 10:52:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🎈saseuwilif")
+st.write("kalkulator pintar berbasis website")
